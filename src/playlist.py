2d45f6f8949f8eff90d4f57c0b6c52933dbd8248
Lyuboslav Karev <lyubo.karev@gmail.com>
2022-12-31 12:07:16 +0200

    Improved code quality of Playlist

--- src/playlist.py
+++ src/playlist.py
@@ -9,19 +9,34 @@ from src.song import Song
 
 
 class Playlist:
+    """
+    Contains all the needed information about a playlist.
+    """
     def __init__(self, name: str, uri: str) -> None:
         self.__name = name
         self.__uri = uri
 
     @property
     def name(self) -> str:
+        """
+        Returns the name of the playlist.
+        """
         return self.__name
-    
+
     @property
     def uri(self) -> str:
+        """
+        Returns the Spotify URI for the playlist.
+        """
         return self.__uri
 
     def get_playlist_tracks(self, client: spotipy.Spotify) -> List[Song]:
+        """
+        Returns a list of songs in the playlist.
+
+        :param client: The Spotify client.
+        :return: A list of Song objects.
+        """
         year = int(self.name.split(' ')[-1])
         results = client.playlist_tracks(self.uri)
         tracks = results['items']
@@ -29,17 +44,25 @@ class Playlist:
             results = client.next(results)
             tracks.extend(results['items'])
 
-        tracks = [Song(track['track']['id'], track['track']['name'], year, i+1) for i, track in enumerate(tracks)]
+        tracks = [Song(track['track']['id'], track['track']['name'], year, i+1)
+                  for i, track in enumerate(tracks)]
         return tracks
 
 
 def get_wrapped_playlists(client: spotipy.Spotify, username: str) -> List[Playlist]:
+    """
+    Returns a list of all Spotify Wrapped playlists for a user.
+
+    :param client: The Spotify client.
+    :param username: The Spotify username.
+    :return: A list of Playlist objects.
+    """
     playlists = client.user_playlists(username)
 
     wrapped_playlists = []
 
     while playlists:
-        for i, playlist in enumerate(playlists['items']):
+        for playlist in playlists['items']:
             if 'Top Songs' in playlist['name']:
                 wrapped_playlists.append(Playlist(playlist['name'], playlist['uri']))
         if playlists['next']:
