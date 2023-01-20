4f5298a13dbabe9231f3a6c4d2664e1f2db4ff6f
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-01-21 00:06:14 +0200

    Song does not break if weight delimiter is 0

--- src/song.py
+++ src/song.py
@@ -51,7 +51,7 @@ class Song:
 
         if rank_score <= 0:
             raise ValueError('Ranking must be between 1 and 100')
-        
+
         # The more recent the playlist, the more weight it has
         # e.g. for the range [2018, 2022]
         # 5 years => 0.20 weight per year
@@ -62,11 +62,17 @@ class Song:
         # 2019 playlist has 0.40x (2 x 0.20) weight
         # 2018 playlist has 0.20x (1 x 0.20) weight
 
-        weight = 1 / (2022 - min_year + 1)
+        # TODO - Replace 2022 with max year
+        weight_delimiter = (2022 - min_year + 1)
+
+        if weight_delimiter == 0:
+            raise ValueError('Weight must be between 0 and 1')
+
+        weight = 1 / weight_delimiter
 
         if not 0 <= weight <= 1:
             raise ValueError('Weight must be between 0 and 1')
-        
+
         year_multiplier = (self.year - min_year + 1) * weight
 
         if year_multiplier <= 0:
