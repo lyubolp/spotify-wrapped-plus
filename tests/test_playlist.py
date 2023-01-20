fe21f93c934a0b19e3e3ec9b940488ef6ca5251c
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-01-21 00:07:24 +0200

    Improved tests

--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -1,20 +1,35 @@
+"""
+Unit tests for the Playlist class
+"""
+
 import unittest
 
 from abc import ABC, abstractmethod
-from src.playlist import Playlist, ClientNotAvailableError
+from src.playlist import Playlist, ClientError
 from src.song import Song
 
+
 class SpotifyStubInterface(ABC):
+    """
+    Interface for the Spotify client stub.
+    """
     @abstractmethod
     def playlist_tracks(self, uri: str):
-        pass
-    
+        """
+        Returns a list of songs in the playlist.
+        """
+
     @abstractmethod
     def next(self, results: dict):
-        pass
+        """
+        Returns the next page of results.
+        """
 
 
 class SpotifyStub(SpotifyStubInterface):
+    """
+    Stub for the Spotify API.
+    """
     def __init__(self):
         self.__songs = [
             {
@@ -23,7 +38,7 @@ class SpotifyStub(SpotifyStubInterface):
                     {'track': {'id': '2', 'name': 'song2'}},
                 ],
                 'next': 1,
-            }, 
+            },
             {
                 'items': [
                     {'track': {'id': '3', 'name': 'song3'}},
@@ -44,6 +59,9 @@ class SpotifyStub(SpotifyStubInterface):
 
 
 class SpotifyStubInvalidResult(SpotifyStubInterface):
+    """
+    Spotify stub that returns an invalid result.
+    """
     def __init__(self, playlist_tracks_to_return, next_to_return):
         self.__playlist_tracks_to_return = playlist_tracks_to_return
         self.__next_to_return = next_to_return
@@ -56,23 +74,51 @@ class SpotifyStubInvalidResult(SpotifyStubInterface):
 
 
 class TestPlaylist(unittest.TestCase):
+    """
+    Unit tests for the Playlist class.
+    """
     def test_01_test_instance(self):
+        """
+        Asserts that the Playlist class is instantiated correctly.
+        """
         # Arrange
+        dummy_name = 'dummy_name'
+        dummy_uri = 'dummy_uri'
+
         # Act
+        has_raised_exception = False
+        try:
+            playlist = Playlist(dummy_name, dummy_uri)
+        except Exception:
+            has_raised_exception = True
+
         # Assert
-        pass
+        self.assertFalse(has_raised_exception)
+        self.assertIsInstance(playlist, Playlist)
 
     def test_02_test_properties(self):
+        """
+        Assert that the properties of the Playlist class are working correctly.
+        """
         # Arrange
+        dummy_name = 'dummy_name'
+        dummy_uri = 'dummy_uri'
+
         # Act
+        playlist = Playlist(dummy_name, dummy_uri)
+
         # Assert
-        pass
+        self.assertEqual(playlist.name, dummy_name, 'The name property is not correct')
+        self.assertEqual(playlist.uri, dummy_uri, 'The uri property is not correct')
 
     def test_03_test_get_playlist_tracks(self):
+        """
+        Assert that the get_playlist_tracks method is working correctly.
+        """
         # Arrange
         dummy_year = 2020
-        dummy_name = f"dummy_name {dummy_year}"
-        dummy_uri = "dummy_uri"
+        dummy_name = f'dummy_name {dummy_year}'
+        dummy_uri = 'dummy_uri'
 
         playlist = Playlist(dummy_name, dummy_uri)
         client = SpotifyStub()
@@ -89,12 +135,16 @@ class TestPlaylist(unittest.TestCase):
         tracks = playlist.get_playlist_tracks(client)
 
         # Assert
-        self.assertEqual(tracks, expected_songs, "Returned songs don't match")
+        self.assertEqual(tracks, expected_songs, 'Returned songs don\'t match')
 
     def test_04_get_playlist_tracks_invalid_playlist_name(self):
+        """
+        Assert that the get_playlist_tracks method raises an exception
+            when the playlist name is invalid.
+        """
         # Arrange
-        wrong_name = "Spotify Wrapped"
-        dummy_uri = "dummy_uri"
+        wrong_name = 'Spotify Wrapped'
+        dummy_uri = 'dummy_uri'
 
         playlist = Playlist(wrong_name, dummy_uri)
         client = SpotifyStub()
@@ -103,21 +153,27 @@ class TestPlaylist(unittest.TestCase):
         with self.assertRaises(ValueError) as ex:
             playlist.get_playlist_tracks(client)
         # Assert
-        self.assertIsInstance(ex.exception, ValueError, "The exception is not of the correct type")
-        self.assertEqual(str(ex.exception), "The playlist name is not valid", "The exception message is not correct")
-    
+        self.assertIsInstance(ex.exception, ValueError, 'The exception is not of the correct type')
+        self.assertEqual(str(ex.exception), 'Playlist name is not valid',
+                         'The exception message is not correct')
+
     def test_05_get_playlist_tracks_none_result(self):
+        """
+        Assert that the get_playlist_tracks method raises an exception
+        """
         # Arrange
-        dummy_name = "dummy_name"
-        dummy_uri = "dummy_uri"
+        dummy_name = 'Spotify Wrapped 2019'
+        dummy_uri = 'dummy_uri'
         playlist = Playlist(dummy_name, dummy_uri)
 
         client = SpotifyStubInvalidResult(None, None)
 
         # Act
-        with self.assertRaises(ClientNotAvailableError) as ex:
+        with self.assertRaises(ClientError) as ex:
             playlist.get_playlist_tracks(client)
-        
+
         # Assert
-        self.assertIsInstance(ex.exception, ClientNotAvailableError, "The exception is not of the correct type")
-        self.assertEqual(str(ex.exception), "The Spotify client returned an error", "The exception message is not correct")
+        self.assertIsInstance(ex.exception, ClientError,
+                              'The exception is not of the correct type')
+        self.assertEqual(str(ex.exception), 'The Spotify client returned an error',
+                         'The exception message is not correct')
--- tests/test_song.py
+++ tests/test_song.py
@@ -1,12 +1,22 @@
+"""
+Module containing the unittests for the Song class.
+"""
 import unittest
 from src.song import Song
 
+
 class TestsSongs(unittest.TestCase):
-    def __create_dummy_instance(self, dummy_id="dummy_id", dummy_name="dummy_name", 
+    """
+    Class containing the unittests for the Song class.
+    """
+    def __create_dummy_instance(self, dummy_id="dummy_id", dummy_name="dummy_name",
                                 dummy_year=2020, dummy_ranking=1):
         return Song(dummy_id, dummy_name, dummy_year, dummy_ranking)
-    
+
     def test_01_create_instance(self):
+        """
+        Verify that the Song class can be instantiated.
+        """
         # Arrange
         dummy_id = "dummy_id"
         dummy_name = "dummy_name"
@@ -23,8 +33,11 @@ class TestsSongs(unittest.TestCase):
         # Assert
         self.assertFalse(has_raised_exception)
         self.assertIsInstance(song, Song)
-    
+
     def test_02_test_properties(self):
+        """
+        Verify that the properties of the Song class are set correctly.
+        """
         # Arrange
         expected_id = "dummy_id"
         expected_name = "dummy_name"
@@ -41,6 +54,9 @@ class TestsSongs(unittest.TestCase):
         self.assertEqual(song.ranking, expected_ranking)
 
     def test_03_test_calculate_score(self):
+        """
+        Verify that the calculate_score method returns the correct score.
+        """
         # Arrange
         dummy_min_year = 2018
         song = self.__create_dummy_instance()
@@ -58,37 +74,82 @@ class TestsSongs(unittest.TestCase):
         self.assertAlmostEqual(score, expected_score, places=6, msg="The score is not correct!")
 
     def test_04_test_calculate_score_with_big_ranking(self):
+        """
+        Assert that the calculate_score method raises a ValueError when the ranking is too big.
+        """
         # Arrange
         big_ranking = 102
         dummy_min_year = 2018
         song = self.__create_dummy_instance(dummy_ranking=big_ranking)
 
-        # year_multiplier by default is 0.6, rank_score will be -1
-        expected_score = -0.6
-        
         # Act
-        score = song.calculate_score(dummy_min_year)
+        with self.assertRaises(ValueError) as ex:
+            song.calculate_score(dummy_min_year)
 
         # Assert
-        self.assertTrue(score > 0, msg="The calculated score should be positive!")
+        self.assertEqual(str(ex.exception), "Ranking must be between 1 and 100",
+                         msg="The error message is not correct!")
 
     def test_05_test_calcualte_score_with_2023_as_min_year(self):
+        """
+        Assert that the calculate_score method raises a ValueError when min year is 2023.
+        """
         # Arrange
+        dummy_min_year = 2023
+        song = self.__create_dummy_instance()
+
         # Act
+        with self.assertRaises(ValueError) as ex:
+            song.calculate_score(dummy_min_year)
         # Assert
-        pass
+        self.assertEqual(str(ex.exception), "Weight must be between 0 and 1",
+                         msg="The error message is not correct!")
+
     def test_06_test_calculate_score_with_2024_as_min_year(self):
+        """
+        Assert that the calculate_score method raises a ValueError when weight is negative.
+        """
         # Arrange
+        dummy_min_year = 2024
+        song = self.__create_dummy_instance()
+
         # Act
+        with self.assertRaises(ValueError) as ex:
+            song.calculate_score(dummy_min_year)
         # Assert
-        pass
+        self.assertEqual(str(ex.exception), "Weight must be between 0 and 1",
+                         msg="The error message is not correct!")
+
     def test_07_test_calculate_score_with_negative_as_year(self):
+        """
+        Assert that the calculate_score method raises a ValueError when year is negative.
+        """
         # Arrange
+        dummy_year = -1
+        song = self.__create_dummy_instance(dummy_year=dummy_year)
+
         # Act
+        with self.assertRaises(ValueError) as ex:
+            song.calculate_score(2018)
+
         # Assert
-        pass
+        self.assertEqual(str(ex.exception), "Year multiplier must be greater than 0",
+                         msg="The error message is not correct!")
+
     def test_08_test_calculate_score_with_year_equal_to_min_year_minus_one(self):
+        """
+        Assert that the calculate_score method raises a ValueError
+            when year is equal to min year - 1.
+        """
         # Arrange
+        dummy_year = 2017
+        dummy_min_year = 2018
+        song = self.__create_dummy_instance(dummy_year=dummy_year)
+
         # Act
+        with self.assertRaises(ValueError) as ex:
+            song.calculate_score(dummy_min_year)
+
         # Assert
-        pass
+        self.assertEqual(str(ex.exception), "Year multiplier must be greater than 0",
+                         msg="The error message is not correct!")
